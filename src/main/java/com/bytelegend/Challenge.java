0e4738ba333322fbc1148b97f1044dc55467c68c
4consultant <996158465@qq.com>
2022-05-24 11:31:00 +0000

    Update at 2022-05-24T11:31:00.792479Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -11,11 +11,11 @@ public class Challenge {
     // Return "a<b" if a < b, "a>b" if a > b, "a=b" otherwise
     public static String relation(int a, int b) {
         if (a > b) {
-            String sign = ">";
+            public String sign = ">";
         } else if (a < b) {
-            String sign = "<";
+            public String sign = "<";
         } else {
-            String sign = "=";
+            public String sign = "=";
         }
 
         return "" + a + sign + b;
