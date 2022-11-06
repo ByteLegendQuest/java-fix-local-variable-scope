aa1e80fdd22af6d721972b946cf9f1f4056d340f
magicalmuggle <magicalmuggle@foxmail.com>
2022-11-06 11:05:43 +0000

    Update at 2022-11-06T11:05:43.062521Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -10,7 +10,7 @@ public class Challenge {
     // Fix the compilation error
     // Return "a<b" if a < b, "a>b" if a > b, "a=b" otherwise
     public static String relation(int a, int b) {
-        String sign；
+        String sign;
         if (a > b) {
              sign = ">";
         } else if (a < b) {
